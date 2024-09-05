7b32c281ca9f5d86417d491b0dfac4b6b942d421
Ameer <tmsyameer@gmail.com>
2024-09-05 21:36:21 +0530

    sameSite reset None on userControllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -19,7 +19,7 @@ const addUser = async (req, res) =>{
     const token = jwt.sign(
         { _id: user._id, email: user.email, name: user.name },  
         process.env.TOKEN_SECRET );  
-    res.cookie('token', token, { httpOnly: true, secure: process.env.ENVIRONMENT === 'development' ? false : true, sameSite: none, maxAge: 1*60*60*1000})
+    res.cookie('token', token, { httpOnly: true, secure: process.env.ENVIRONMENT === 'development' ? false : true, maxAge: 1*60*60*1000, sameSite: 'None'})
     res.status(202).json({_id: user._id, email: user.email, name: user.name})
 }
 
